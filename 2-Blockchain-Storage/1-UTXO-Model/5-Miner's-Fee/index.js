cb4d6adf45ad0b5865043cca6d7284c2797df90f
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-04-19 22:52:41 -0400

    Create index.js

---
+++ 2-Blockchain-Storage/1-UTXO-Model/5-Miner's-Fee/index.js
@@ -0,0 +1,29 @@
+class Transaction {
+    constructor(inputUTXOs, outputUTXOs) {
+        this.inputUTXOs = inputUTXOs;
+        this.outputUTXOs = outputUTXOs;
+        this.fee = 0;
+    }
+    execute() {
+        let inputUTXOsAmount = 0;
+        let outputUTXOsAmount = 0;
+        this.inputUTXOs.forEach(tx => {
+            if (tx.spent === true) {
+                throw new Error('Already Spent!');
+            }
+            inputUTXOsAmount += tx.amount;
+        })
+        this.outputUTXOs.forEach(tx => {
+            outputUTXOsAmount += tx.amount;
+        })
+        if (outputUTXOsAmount > inputUTXOsAmount) {
+            throw new Error('Input less than Output')
+        }
+        this.inputUTXOs.forEach(tx => {
+            tx.spent = true;
+        })
+        this.fee = inputUTXOsAmount - outputUTXOsAmount;
+    }
+}
+
+module.exports = Transaction;
