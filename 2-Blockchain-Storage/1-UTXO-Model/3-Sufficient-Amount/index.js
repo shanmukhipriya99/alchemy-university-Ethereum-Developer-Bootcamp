25bd00b4679f3f46c14e01a9044da2b62120e360
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-04-19 22:43:48 -0400

    Create index.js

---
+++ 2-Blockchain-Storage/1-UTXO-Model/3-Sufficient-Amount/index.js
@@ -0,0 +1,24 @@
+class Transaction {
+    constructor(inputUTXOs, outputUTXOs) {
+        this.inputUTXOs = inputUTXOs;
+        this.outputUTXOs = outputUTXOs;
+    }
+    execute() {
+        let inputUTXOsAmount = 0;
+        let outputUTXOsAmount = 0;
+        this.inputUTXOs.forEach(tx => {
+            if (tx.spent === true) {
+                throw new Error('Already Spent!');
+            }
+            inputUTXOsAmount += tx.amount;
+        })
+        this.outputUTXOs.forEach(tx => {
+            outputUTXOsAmount += tx.amount;
+        })
+        if (outputUTXOsAmount > inputUTXOsAmount) {
+            throw new Error('Input less than Output')
+        }
+    }
+}
+
+module.exports = Transaction;
