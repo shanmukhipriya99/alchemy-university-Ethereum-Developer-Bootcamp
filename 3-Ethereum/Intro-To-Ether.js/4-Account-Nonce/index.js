40d014a18cd231dd1870caa7ecc505efa288f772
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-04 20:10:42 -0400

    Create index.js

---
+++ 3-Ethereum/Intro-To-Ether.js/4-Account-Nonce/index.js
@@ -0,0 +1,25 @@
+const { Wallet, utils, providers } = require('ethers');
+const { ganacheProvider, PRIVATE_KEY } = require('./config');
+
+// TODO: replace undefined with a new web3 provider
+const provider = new providers.Web3Provider(ganacheProvider);
+
+const wallet = new Wallet(PRIVATE_KEY, provider);
+
+async function sendEther({ value, to }) {
+    // const rawTx = await wallet.signTransaction({ 
+    //     value, to, 
+    //     gasLimit: 0x5208,
+    //     gasPrice: 0x3b9aca00 
+    // });
+
+    // TODO: send the transaction and return the transaction promise
+    // return (await provider.sendTransaction(rawTx));
+    return (await wallet.sendTransaction({
+        value, to,
+        gasLimit: 0x5208,
+        gasPrice: 0x3b9aca00
+    }));
+}
+
+module.exports = sendEther;
